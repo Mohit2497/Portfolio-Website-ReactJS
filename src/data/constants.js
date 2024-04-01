8474896e791b44a2f37c5debc29e247a8131c081
Mohit Deshpande <mohitdeshpande2497@gmail.com>
2024-04-01 11:51:59 -0700

    First Commit

---
+++ src/data/constants.js
@@ -0,0 +1,287 @@
+export const Bio = {
+    name: "Mohit Deshpande",
+    roles: [
+      "Data Analyst",
+      "Business Analyst",
+      "Database Developer"
+    ],
+    description:
+      "With 2 years of professional experience as a Data Analyst at Google, I hold a Bachelor's degree in Information Technology and currently I am pursuing a Master's degree in Data Analytics Engineering from Northeastern University. Passionate about leveraging data, I thrive in projects requiring robust analysis and visualization. Proficient in Python, SQL, and Tableau, I bring a keen eye for detail and a penchant for collaborative work. Experienced in handling large datasets, I am driven by challenges and dedicated to delivering high-quality results. As a motivated and adaptable individual, I constantly seek opportunities to expand my skill set and contribute effectively to team objectives. With a commitment to excellence, I am poised to tackle the complexities of data analysis and eager to embark on new professional endeavors.",
+    github: "https://github.com/Mohit2497",
+    resume:
+      "https://drive.google.com/file/d/1Mdl_quLqf8dtKaobhjjYR7USX9T4LuqG/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/deshpandem/ ",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+       
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+          url: "https://www.w3schools.com/html/"
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+          url: "https://www.w3schools.com/css/"
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+          url: "https://www.javascript.com/"
+        },
+        {
+          name: "React Js",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+          url: "https://reactjs.org/"
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+          url: "https://www.mysql.com/"
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+          url: "https://www.postgresql.org/"
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+          url: "https://www.mongodb.com/"
+        },
+      ],
+    },
+    {
+      title: "Visualization Tools",
+      skills: [
+        {
+          name: "Tableau",
+          image:
+            "https://www.svgrepo.com/show/354428/tableau-icon.svg",
+          url: "https://www.tableau.com/"
+        },
+        {
+          name: "PowerBI",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/600px-New_Power_BI_Logo.svg.png?20210102182532",
+          url: "https://www.microsoft.com/en-us/power-platform/products/power-bi"
+        },
+        {
+          name: "Google Data Studio",
+          image:
+            "https://svglogos.net/wp-content/uploads/google-data-studio.svg",
+          url: "https://lookerstudio.google.com/u/0/navigation/reporting"
+        },
+        {
+          name: "Flourish",
+          image:
+            "https://flourish.studio/images/flourish-icon-112x112.png",
+          url: "https://flourish.studio/"
+        },
+      ],
+    },
+    {
+      title: "Excel & SpreadSheets",
+      skills: [
+        {
+          name: "Excel",
+          image:
+            "https://www.logo.wine/a/logo/Microsoft_Excel/Microsoft_Excel-Logo.wine.svg",
+          url: "https://www.office.com/"
+        },
+        {
+          name: "SpreadSheets",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/3/30/Google_Sheets_logo_%282014-2020%29.svg",
+          url: "https://docs.google.com/spreadsheets/"
+        },
+      ],
+    },
+    {
+      title: "Technologies/Frameworks",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg",
+          url: "https://git-scm.com/"
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+          url: "https://github.com/Mohit2497"
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+          url: "https://www.docker.com/"
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+          url: "https://www.netlify.com/"
+        },
+        {
+          name: "Linux",
+          image:
+            "https://www.logo.wine/a/logo/Linux/Linux-Logo.wine.svg",
+          url: "https://www.linux.org/"
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://www.svgrepo.com/show/380993/google-logo-search-new.svg",
+      url: "https://www.linkedin.com/in/deshpandem/",
+      role: "Data Analyst",
+      company: "Google",
+      date: "Sept 2021 - July 2023",
+      desc: "During my tenure at Google, I collaborated closely with cross-functional teams to conceptualize and implement a centralized vendor access management system, significantly reducing provisioning time from 48 hours to just 4-6 hours, marking a remarkable 90% improvement. Leveraging complex SQL queries and JOIN operations, I established robust relationships among six data tables, enhancing data connectivity across multiple spreadsheets and enabling the periodic update of data every 24 hours. Through the strategic implementation of Python scripts, I achieved a notable 65% enhancement in client onboarding processes, optimizing workflows, minimizing data duplication, and establishing clear project guidelines. Working alongside data owners and stakeholders from various product areas, I spearheaded efforts to validate and verify data accuracy, implementing rigorous processes that led to a 70% reduction in inconsistencies, duplications, and inaccuracies. Additionally, I integrated data from four databases using SQL to develop a real-time dashboard in Google Data Studio, offering comprehensive insights into 30 client locations and capturing key metrics. Through proactive maintenance and updates, I successfully decreased SQL script-related issues by 12% and boosted script performance by 20%, resulting in enhanced productivity and streamlined workflows across the board.",
+      skills: [
+        "SQL",
+        "Python",
+        "SpreadSheets",
+        "Google Data Studio",
+      ],
+    },
+    {
+      id: 0,
+      img: "https://media-exp1.licdn.com/dms/image/C560BAQFgHU3sTF4LfQ/company-logo_200_200/0/1519895156650?e=2159024400&v=beta&t=1iqBaESC2l4UUW7JjEjq0R_HQhwRTaaqyQG1k46q4bs",
+      url: "https://www.linkedin.com/in/deshpandem/",
+      role: "Datasciene Intern",
+      company: "The SPARKS Foundation Internship",
+      date: "Apr 2021 - May 2021",
+      desc: "I applied both supervised and unsupervised machine learning techniques to the provided datasets, aligning with task requirements to predict desired outcomes effectively. Additionally, I conducted thorough exploratory data analysis utilizing various Python libraries, extracting valuable insights from the datasets to inform decision-making processes.",
+      skills: [
+        "Python",
+        "Pandas",
+        "Matplotlib",
+        "Seaborn",
+        "Scikit-learn",
+        "Numpy",
+      ],
+      doc: "https://media.licdn.com/dms/image/D4D2DAQGcypDuYWcVGw/profile-treasury-image-shrink_800_800/0/1686773595304?e=1712358000&v=beta&t=7lRsul1MzWJuLpxyeg9F0Gvz1-WKuAEv58r2CArX9Ug", 
+      doc1: "https://media.licdn.com/dms/image/D4D2DAQEkSRFBlNML9Q/profile-treasury-image-shrink_800_800/0/1686773399669?e=1712358000&v=beta&t=7lstBVytP3lrH-r_uyUfKyVoSQKE5p7Mh6vsMzNMfDY",
+    },
+    {
+      id: 1,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Star_Television_logo.svg/320px-Star_Television_logo.svg.png",
+      url: "https://www.linkedin.com/in/deshpandem/",
+      role: "Database Entry Intern",
+      company: "STAR India Pvt Ltd.",
+      date: "Mar 2019 - Jun 2019",
+      desc: "Translated unstructured data and information from more than 500 Excel sheets by entering data into a newly designed system for the advertising sales team.",
+      skills: [
+        "SQL",
+        "Excel",
+        "VBA",
+        "Macros",
+      ],
+    },
+
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://th.bing.com/th/id/OIP.8YxhvG_EdsWhVcvhsH0RZwHaHa?rs=1&pid=ImgDetMain",
+      school: "Northeastern University, Vancouver, Canada",
+      date: "Sept 2023 - May 2025",
+      grade: "3.8 GPA",
+      degree: "Masters of Science in Data Analytics Engineering",
+    },
+    {
+      id: 1,
+      img: "https://th.bing.com/th/id/OIP.qigHIHsXp55eQ4kUWFB9qgAAAA?rs=1&pid=ImgDetMain",
+      school: "Xavier Institute of Engineering, Mumbai",
+      date: "Jul 2015 - Jun 2021",
+      grade: "3.31 / 4.00 GPA",
+      degree: "Bachelors's of Engineering in Information Technology",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "UK Road Accident Analysis",
+      date: "Nov 2023 - Dec 2023",
+      description:
+        "This project contains analysis and visualizaton of the data for the road accidents in the United Kingdom from 2019 to 2022.The analysis is presented through a Tableau dashboard, providing insights into various aspects of road accidents, including trends, severity, and contributing factors.",
+      image:"https://firebasestorage.googleapis.com/v0/b/imageuploaddb-89e94.appspot.com/o/Project_1.png?alt=media&token=c3b769d5-00e8-4e80-b552-8ab0608bcd4d",
+      tags: [
+        "Tableau",
+        "Python",
+        "Excel",
+      ],
+      category: "data analytics",
+      github: "https://github.com/Mohit2497/UK-Road-Accident-Analysis",
+      webapp: "https://public.tableau.com/views/RoadAccidentAnalysis_17020115277720/Dashboard1?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link",
+    },
+    {
+      id: 0,
+      title: "Suicide Rate Analysis",
+      date: "Sept 2023 - Oct 2023",
+      description:
+        "A small visualization project on Suicide Rate Analysis, where we explore and analyze suicide rates from 1985 to 2016 using Flourish-created charts. Additionally, a comprehensive Google Site is available for a more in-depth exploration of the analysis and insights derived from the dataset.",
+      image: "https://firebasestorage.googleapis.com/v0/b/imageuploaddb-89e94.appspot.com/o/Project_2.png?alt=media&token=f3ed6f84-1758-4172-ab9c-4292838162af",
+      tags: ["Flourish Studio", "Google Sites", "SpreadSheets"],
+      category: "data analytics",
+      github: "https://github.com/Mohit2497/Suicide-Rate-Analysis_1985-2016",
+      webapp: "https://sites.google.com/view/mohitdeshpande-ie6600/home?authuser=0",
+    },
+    {
+      id: 1,
+      title: "Steganography Tool",
+      date: "Jun 2020 - May 2021",
+      description:"This Steganography tool can be use for hiding messages within Image , Audio and Video files.",
+      image:
+        "https://github.com/Mohit2497/Steganography-Tool/blob/main/Input/img.png?raw=true",
+      tags: [
+        "Python"
+      ],
+      category: "machine learning",
+      github: "https://github.com/Mohit2497/Steganography-Tool",
+      member: [
+        {
+          name: "Mohit Deshpande",
+          img: "https://avatars.githubusercontent.com/u/68893690?v=4",
+          linkedin: "https://www.linkedin.com/in/deshpandem/",
+          github: "https://github.com/Mohit2497",
+        },
+        {
+          name: "Rohan Thorat",
+          img: "https://avatars.githubusercontent.com/u/29408235?v=4",
+          linkedin: "https://www.linkedin.com/in/rohan-thorat-8651551a1/",
+          github: "https://github.com/Zorrat",
+        },
+      ],
+    },
+  ];
+  
+  // export const TimeLineData = [
+  //   { year: 2017, text: "Started my journey" },
+  //   { year: 2018, text: "Worked as a freelance developer" },
+  //   { year: 2019, text: "Founded JavaScript Mastery" },
+  //   { year: 2020, text: "Shared my projects with the world" },
+  //   { year: 2021, text: "Started my own platform" },
+  // ];
